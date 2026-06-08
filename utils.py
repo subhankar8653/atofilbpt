a664ff7b95281717bf2beb42026dea2b60039a60
subhankar8653 <163716142+subhankar8653@users.noreply.github.com>
2026-06-08 23:30:12 +0530

    Update bot owner link in utils.py

--- utils.py
+++ utils.py
@@ -831,7 +831,7 @@ async def send_all(bot, userid, files, ident, chat_id, user_name, query):
                                 InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
                                 InlineKeyboardButton('Uᴘᴅᴀᴛᴇs Cʜᴀɴɴᴇʟ', url=CHNL_LNK)
                             ],[
-                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/cosmic_freak")
+                                InlineKeyboardButton("Bᴏᴛ Oᴡɴᴇʀ", url="t.me/suhanibots")
                                 ]
                             ]
                         )
